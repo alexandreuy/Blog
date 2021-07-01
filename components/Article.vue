c71a23c8e32dfc8ce2e30f37c070ee92ed8ce7af
Alexandre <alexandre.uy@jack.agency>
2021-07-01 12:33:25 +0200

    first commit

---
+++ components/Article.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2> {{ title }}</h2>
+        <p>{{ contenu }} </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Article',
+        props: ["title", "contenu"]
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nuxt-link to="/"> Accueil </nuxt-link>
+        <Article title="Hello" contenu="World" />
+    </div>
+</template>
+
+<script>
+import Article from '../../components/Article.vue'
+    export default {
+        name: 'Blog',
+        components:{
+           Article 
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
